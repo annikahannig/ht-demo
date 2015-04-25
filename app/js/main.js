2412140a67408cbf5bbaba47239a1b66cfdbce4a
Matthias Hannig <matthias@hannig.cc>
2015-04-26 00:07:42 +0200

    bouncing ball and hi there.

--- app/js/main.js
+++ app/js/main.js
@@ -8,17 +8,20 @@
 var D = function(canvas) {
   this.canvas = canvas;
   this.ctx    = canvas.getContext('2d');
-
+  this.cx = 80;
+  this.cy = 100;
+  this.incX = 4;
+  this.incY = -4;
 };
 
 D.prototype.resize = function() {
   var self = this;
 
-  width = window.innerWidth
-  height = window.innerHeight - 100
-
+  var width = window.innerWidth - 20;
+  var height = window.innerHeight - 20;
   self.ctx.canvas.width  = width
   self.ctx.canvas.height = height
+
 };
 
 D.prototype.drawCircleAt = function(x,y,r) {
@@ -26,18 +29,47 @@ D.prototype.drawCircleAt = function(x,y,r) {
   self.ctx.beginPath();
   self.ctx.fillStyle = "#ffffff";
   self.ctx.arc(x, y, r, 0, 2*Math.PI);
+  self.ctx.fill();
+};
+
+D.prototype.animateCircle = function() {
+  var self = this;
+  // bounce
+  if( self.cx > self.canvas.width - 50 ) {
+    self.incX = -10;  
+  }
+  if( self.cy > self.canvas.height - 50 ) {
+    self.incY = -10;  
+  }
+  if(self.cx <= 40){
+    self.incX = 10;
+  }
+  if(self.cy <= 40) {
+    self.incY = 10;
+  }
+
+  self.cx += self.incX; self.cy += self.incY;
+
+  // pulse
+  var r = 40 + Mth.pulse( 40, (self.t*0.0003) % 0.6 )*40;
+  self.drawCircleAt(self.cx, self.cy, r);
 };
 
 D.prototype.render = function() {
   var self = this;
 
-  self.ctx.fillStyle = "rgba(0, 0, 0, 0.75)"; // motionblur
+  self.ctx.fillStyle = "rgba(0, 0, 0, 0.5)"; // motionblur
   self.ctx.fillRect(0, 0, self.canvas.width, self.canvas.height);
+
+  self.animateCircle();
 };
 
-D.prototype.animate = function() {
+D.prototype.animate = function(t) {
   var self = this;
-  window.requestAnimationFrame(self.animate);
+  window.requestAnimationFrame(function(t) {
+    self.t = t;
+    self.animate()
+  });
   self.render();
 };
 
@@ -45,7 +77,7 @@ D.prototype.run = function() {
   var self = this;
   self.t = 0;
   self.resize();
-  self.render();
+  self.animate(0);
 };
 
 
