c2128a4af8c0954559b257506d8ca6cf8489b930
Matthias Hannig <matthias@hannig.cc>
2015-04-26 00:35:22 +0200

    added starfield structure.

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,49 @@
  */
 
 
+var Star = function(ctx) { 
+  this.ctx = ctx;
+  this.canvas = ctx.canvas;
+  this.reset();
+};
+
+Star.prototype.reset = function() {
+  var self = this;
+
+  self.x = 2*(Math.random()*self.canvas.width)  - self.canvas.width;
+  self.y = 2*(Math.random()*self.canvas.height) - self.canvas.height;
+  self.z = 0.5*(self.canvas.width+self.canvas.height);
+};
+
+Star.prototype.draw = function() {
+  var self = this;
+};
+
+var Starfield = function(ctx, starcount) {
+  this.ctx = ctx;
+  this.canvas = ctx.canvas;
+
+  this.starcount = starcount;
+
+  this.stars = [];
+  for(var i = 0; i < starcount; i++){
+    this.stars.push(new Star(ctx));
+  }
+};
+
+
+var Starfield.prototype.draw = function() {
+  var self = this; 
+  for(var i = 0; i < self.starcount; i++) {
+    self.stars[i].draw();
+  }
+}
+
+// .::..::.
+
 var D = function(canvas) {
   this.canvas = canvas;
   this.ctx    = canvas.getContext('2d');
-  this.cx = 80;
-  this.cy = 100;
-  this.incX = 4;
-  this.incY = -4;
 };
 
 D.prototype.resize = function() {
@@ -22,38 +58,9 @@ D.prototype.resize = function() {
   self.ctx.canvas.width  = width
   self.ctx.canvas.height = height
 
+  self.sf = new Starfield(this.ctx, 500);
 };
 
-D.prototype.drawCircleAt = function(x,y,r) {
-  var self = this;
-  self.ctx.beginPath();
-  self.ctx.fillStyle = "#ffffff";
-  self.ctx.arc(x, y, r, 0, 2*Math.PI);
-  self.ctx.fill();
-};
-
-D.prototype.animateCircle = function() {
-  var self = this;
-  // bounce
-  if( self.cx > self.canvas.width - 50 ) {
-    self.incX = -10;  
-  }
-  if( self.cy > self.canvas.height - 50 ) {
-    self.incY = -10;  
-  }
-  if(self.cx <= 40){
-    self.incX = 10;
-  }
-  if(self.cy <= 40) {
-    self.incY = 10;
-  }
-
-  self.cx += self.incX; self.cy += self.incY;
-
-  // pulse
-  var r = 40 + Mth.pulse( 40, (self.t*0.0003) % 0.6 )*40;
-  self.drawCircleAt(self.cx, self.cy, r);
-};
 
 D.prototype.render = function() {
   var self = this;
