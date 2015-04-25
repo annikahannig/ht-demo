75f4cd16a073f2ac7ce6fee5a333d01d6e150add
Matthias Hannig <matthias@hannig.cc>
2015-04-26 01:08:39 +0200

    nothing works *sigh*

--- app/js/main.js
+++ app/js/main.js
@@ -9,18 +9,22 @@ var Star = function(ctx) {
   this.ctx = ctx;
   this.canvas = ctx.canvas;
   this.reset();
+
+  this.dZ = 0.1;
 };
 
 Star.prototype.reset = function() {
   var self = this;
 
-  self.x = 2*(Math.random()*self.canvas.width)  - self.canvas.width;
-  self.y = 2*(Math.random()*self.canvas.height) - self.canvas.height;
-  self.z = 0.5*(self.canvas.width+self.canvas.height);
+  self.x = -0.5*(Math.random()*self.canvas.width*2)  + self.canvas.width;
+  self.y = -0.5*(Math.random()*self.canvas.height*2) + self.canvas.height;
+  self.z = 1;// Math.random()*4;// 0.5*(self.canvas.width+self.canvas.height);
 };
 
 Star.prototype.draw = function() {
   var self = this;
+  var c = self.ctx;
+
 };
 
 var Starfield = function(ctx, starcount) {
@@ -29,19 +33,32 @@ var Starfield = function(ctx, starcount) {
 
   this.starcount = starcount;
 
-  this.stars = [];
+  this.stars = new Array(starcount);
   for(var i = 0; i < starcount; i++){
-    this.stars.push(new Star(ctx));
+    this.stars[i] = new Star(ctx);
   }
 };
 
-
-var Starfield.prototype.draw = function() {
+Starfield.prototype.animate = function() {
   var self = this; 
   for(var i = 0; i < self.starcount; i++) {
-    self.stars[i].draw();
+    self.stars[i].z -= 0.01;
   }
-}
+};
+
+Starfield.prototype.draw = function() {
+  var self = this; 
+  for(var i = 0; i < self.starcount; i++) {
+    var st = self.stars[i];
+    // project
+    var px = st.x / st.z;
+    var py = st.y / st.z;
+    
+    self.ctx.fillStyle = '#ffffff';
+    self.ctx.rect(px, py, 1,1);
+    self.ctx.fill()
+  }
+};
 
 // .::..::.
 
@@ -58,17 +75,17 @@ D.prototype.resize = function() {
   self.ctx.canvas.width  = width
   self.ctx.canvas.height = height
 
-  self.sf = new Starfield(this.ctx, 500);
 };
 
 
 D.prototype.render = function() {
   var self = this;
 
-  self.ctx.fillStyle = "rgba(0, 0, 0, 0.5)"; // motionblur
+  self.ctx.fillStyle = "rgba(0, 0, 0, 1)"; // motionblur
   self.ctx.fillRect(0, 0, self.canvas.width, self.canvas.height);
 
-  self.animateCircle();
+  self.sf.animate();
+  self.sf.draw();
 };
 
 D.prototype.animate = function(t) {
@@ -84,6 +101,7 @@ D.prototype.run = function() {
   var self = this;
   self.t = 0;
   self.resize();
+  self.sf = new Starfield(this.ctx, 500);
   self.animate(0);
 };
 
