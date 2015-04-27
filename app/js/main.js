c287f24cf059ed4f5024e7a6923540a858765edc
Matthias Hannig <matthias@hannig.cc>
2015-04-27 23:39:25 +0200

    removed legacy code.

--- app/js/main.js
+++ app/js/main.js
@@ -4,67 +4,6 @@
  * [DEMO] by @dlsym
  */
 
-
-
-/*
-
-var Star = function(ctx) { 
-  this.ctx = ctx;
-  this.canvas = ctx.canvas;
-  this.reset();
-
-  this.dZ = 0.1;
-};
-
-Star.prototype.reset = function() {
-  var self = this;
-
-  self.x = -0.5*(Math.random()*self.canvas.width*2)  + self.canvas.width;
-  self.y = -0.5*(Math.random()*self.canvas.height*2) + self.canvas.height;
-  self.z = 1;// Math.random()*4;// 0.5*(self.canvas.width+self.canvas.height);
-};
-
-Star.prototype.draw = function() {
-  var self = this;
-  var c = self.ctx;
-
-};
-
-var Starfield = function(ctx, starcount) {
-  this.ctx = ctx;
-  this.canvas = ctx.canvas;
-
-  this.starcount = starcount;
-
-  this.stars = new Array(starcount);
-  for(var i = 0; i < starcount; i++){
-    this.stars[i] = new Star(ctx);
-  }
-};
-
-Starfield.prototype.animate = function() {
-  var self = this; 
-  for(var i = 0; i < self.starcount; i++) {
-    self.stars[i].z -= 0.01;
-  }
-};
-
-Starfield.prototype.draw = function() {
-  var self = this; 
-  for(var i = 0; i < self.starcount; i++) {
-    var st = self.stars[i];
-    // project
-    var px = st.x / st.z;
-    var py = st.y / st.z;
-    
-    self.ctx.fillStyle = '#ffffff';
-    self.ctx.rect(px, py, 1,1);
-    self.ctx.fill()
-  }
-};
-
-*/
-
 // .::..::.
 
 var D = function(canvas) {
